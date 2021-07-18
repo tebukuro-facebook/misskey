30d887382a0860cc809ea9c121fe457ed78c5a33
tebukuro <43908794+tebukuro-facebook@users.noreply.github.com>
2021-07-18 11:23:17 +0900

    [add] files for local test
    ローカル環境での実行用ファイルを追加

---
+++ run-local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker-compose -f docker-compose-local.yml build
+
+docker-compose -f docker-compose-local.yml run --rm web yarn run init
+
+docker-compose -f docker-compose-local.yml  up -d
+
+docker ps
