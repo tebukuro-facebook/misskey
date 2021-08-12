2850de09bd602942e33c5fcf6d0658fcfcd55446
tebukuro <43908794+tebukuro-facebook@users.noreply.github.com>
2021-08-12 20:53:50 +0900

    [fix] files for local test

--- run-local.sh
+++ run-local.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+docker-compose -f docker-compose-local.yml stop
+
 docker-compose -f docker-compose-local.yml build
 
 docker-compose -f docker-compose-local.yml run --rm web yarn run init
 
-docker-compose -f docker-compose-local.yml  up -d
+docker-compose -f docker-compose-local.yml up -d
 
 docker ps
